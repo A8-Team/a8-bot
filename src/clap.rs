713d83fb0fe47cbbef2ae4b5f09252279c29d0de
JayceLaux <jaycelaux@gmail.com>
2025-04-14 14:35:51 +0800

    chore: 工程模块化（暂停，改为基于grpc手搓）
    - 删除 .env 目录的忽略项

---
+++ src/clap.rs
@@ -0,0 +1,49 @@
+use clap::{Parser, ValueEnum};
+use solana_sdk::commitment_config::CommitmentLevel;
+use yellowstone_grpc_client::{GeyserGrpcBuilder, GeyserGrpcClient, Interceptor};
+use anyhow::Context;
+
+#[derive(Debug, Clone, Parser)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(short, long, default_value_t = String::from("http://127.0.0.1:10000"))]
+    pub grpc_endpoint: String,
+
+    #[clap(long)]
+    pub commitment: Option<ArgsCommitment>,
+    
+    // 最大解码消息大小，完整的块可能会特别大，单位是B，默认值是1GB
+    #[clap(long, default_value_t = 1024*1024*1024)]
+    max_decoding_message_size: usize,
+}
+// 交易确认级别
+#[derive(Debug, Clone, Copy, Default, ValueEnum)]
+pub enum ArgsCommitment {
+    #[default]
+    Processed,
+    Confirmed,
+    Finalized,
+}
+// 为ArgsCommitment实现向CommitmentLevel的转换
+impl From<ArgsCommitment> for CommitmentLevel {
+    fn from(commitment: ArgsCommitment) -> Self {
+        match commitment {
+            ArgsCommitment::Processed => CommitmentLevel::Processed,
+            ArgsCommitment::Confirmed => CommitmentLevel::Confirmed,
+            ArgsCommitment::Finalized => CommitmentLevel::Finalized,
+        }
+    }
+}
+
+impl Args {
+    // 确认交易确认级别
+    fn get_commitment(&self) -> Option<CommitmentLevel> {
+        Some(self.commitment.unwrap_or_default().into())
+    }
+    // 连接
+    async fn connect(&self) -> anyhow::Result<GeyserGrpcClient<impl Interceptor>> {
+        let mut builder = GeyserGrpcClient::build_from_shared(self.grpc_endpoint.clone())?
+            .max_decoding_message_size(self.max_decoding_message_size);
+        builder.connect().await.map_err(Into::into)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod clap;
+
 use core::option::Option;
 use futures::join;
 use jupiter_swap_api_client::quote::{QuoteRequest, QuoteResponse, SwapMode};
@@ -12,90 +14,11 @@ use std::io::Error;
 use std::rc::Rc;
 use tokio::time::{sleep, Duration};
 
-const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
-const SOL_MINT: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
 #[tokio::main]
 async fn main() {
     env_logger::init();
     println!("Starting A8 Bot ...");
     // 加载.env变量
     dotenv::dotenv().ok();
-    let jup_api_url = env::var("JUPITER_API_URL").expect("JUPITER_API_URL Not Define！");
-    let solana_rpc_url = env::var("SOLANA_RPC_URL").expect("SOLANA_RPC_URL Not Defined!");
-    let wall_address = env::var("WALLET_ADDRESS").expect("WALLET_ADDRESS Not Defined!");
-    // 获取报价
-    let jupiter_client = JupiterSwapApiClient::new(jup_api_url);
-
-    loop {
-        let (res1, res2) =
-            get_arbitrage_opportunity(jupiter_client.clone(), SOL_MINT, USDC_MINT, 5000000000)
-                .await;
-
-        if res1.is_ok() && res2.is_ok() {
-            let q1 = res1.unwrap();
-            let q2 = res2.unwrap();
-            println!("计算套利空间:");
-            println!("Q1:{},{}", q1.in_amount, q1.out_amount);
-            println!("Q2:{},{}", q2.in_amount, q2.out_amount);
-            let profit = q1.out_amount as f64 / q2.in_amount as f64 * q2.out_amount as f64
-                - q1.in_amount as f64;
-            println!("套利空间: {}", profit);
-            if profit > 150000.0 {
-                println!("套利空间: {}", profit)
-            } else {
-                println!("不存在套利空间！")
-            }
-        } else {
-            println!("请求失败！");
-        }
-        println!("-------------------------");
-        sleep(Duration::from_secs(1)).await;
-    }
-}
-
-// 获取套利机会
-async fn get_arbitrage_opportunity(
-    client: JupiterSwapApiClient,
-    input_mint: Pubkey,
-    output_mint: Pubkey,
-    amount: u64,
-) -> (
-    Result<QuoteResponse, ClientError>,
-    Result<QuoteResponse, ClientError>,
-) {
-    let (q1, q2) = join!(
-        get_quote(
-            &client,
-            input_mint,
-            output_mint,
-            amount,
-            Option::from(SwapMode::ExactIn)
-        ),
-        get_quote(
-            &client,
-            output_mint,
-            input_mint,
-            amount,
-            Option::from(SwapMode::ExactOut)
-        )
-    );
-    return (q1, q2);
-}
-// 获取报价
-async fn get_quote(
-    client: &JupiterSwapApiClient,
-    input_mint: Pubkey,
-    output_mint: Pubkey,
-    amount: u64,
-    swap_mode: Option<SwapMode>,
-) -> Result<QuoteResponse, ClientError> {
-    let quote_request = QuoteRequest {
-        input_mint,
-        output_mint,
-        amount,
-        swap_mode,
-        slippage_bps: 0,
-        ..QuoteRequest::default()
-    };
-    client.quote(&quote_request).await
+    
 }
